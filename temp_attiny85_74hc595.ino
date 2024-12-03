2f2619683a3a47385c3b0dbc348f8d2aafc6ecbe
pablofranzoni <pablo.franzoni@gmail.com>
2024-12-03 19:33:23 -0300

    Initial commit

---
+++ temp_attiny85_74hc595.ino
@@ -0,0 +1,63 @@
+#include <LiquidCrystalSerial.h>
+
+#define DS    0
+#define ST_CP 1
+#define SH_CP 2
+
+LiquidCrystalSerial lcd(SH_CP, DS, ST_CP);
+    
+// which analog pin to connect
+#define THERMISTORPIN 3         
+// resistance at 25 degrees C
+#define THERMISTORNOMINAL 9830      
+// temp. for nominal resistance (almost always 25 C)
+#define TEMPERATURENOMINAL 25   
+// how many samples to take and average, more takes longer
+// but is more 'smooth'
+#define NUMSAMPLES 5
+// The beta coefficient of the thermistor (usually 3000-4000)
+#define BCOEFFICIENT 3950
+// the value of the 'other' resistor
+#define SERIESRESISTOR 10000    
+
+int samples[NUMSAMPLES];
+  
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("hi, temperature!");
+  delay(1500);
+}
+
+void loop() {
+  uint8_t i;
+  float average;
+  
+  // take N samples in a row, with a slight delay
+  for (i=0; i< NUMSAMPLES; i++) {
+   samples[i] = analogRead(THERMISTORPIN);
+   delay(10);
+  }
+  
+  // average all the samples out
+  average = 0;
+  for (i=0; i< NUMSAMPLES; i++) {
+     average += samples[i];
+  }
+  average /= NUMSAMPLES;
+  
+  // convert the value to resistance
+  average = 1023 / average - 1;
+  average = SERIESRESISTOR / average;
+  
+  float steinhart;
+  steinhart = average / THERMISTORNOMINAL;     // (R/Ro)
+  steinhart = log(steinhart);                  // ln(R/Ro)
+  steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+  steinhart = 1.0 / steinhart;                 // Invert
+  steinhart -= 273.15;                         // convert absolute temp to C
+
+  lcd.setCursor(0, 1);
+  lcd.print(steinhart);
+  delay(10000);
+}
